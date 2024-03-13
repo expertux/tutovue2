30906d7348c8a147476323fce7f3b3302c7d9c11
GF-SIS <info@grupofesa.co>
2024-03-12 22:43:39 -0500

    ultimo cambio en fenix2 donde se ve codigo de contadores por metodo optionapi o composition

--- fenix2/src/App.vue
+++ fenix2/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <h1> Listado de Posts: </h1>
-  <PostContainer />
+    <CounterOption/>
+    <p> aca inicia por composition</p>
+    <CounterComposition/>
+    <p>aca inicia por composition pero menos lienas codigo  resumido</p>
+    <CounterCompositionSmall />
 </template>
 
 <script lang="ts" setup>
-import PostContainer from './components/PostContainer.vue';
+import CounterOption from './components/CounterOption.vue'
+import CounterComposition from './components/CounterComposition.vue'
+import CounterCompositionSmall from './components/CounterCompositionSmall.vue'
 </script>
 
 <style>
---
+++ fenix2/src/components/CounterCompositionSmall.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3>{{ countersmall }}</h3>
+    <button @click="addCounter"> IncrementarSmall</button>
+</template>
+    
+    <script lang="ts" setup>
+        import { Ref, ref} from "vue"
+        let countersmall:Ref = ref(0)
+        const addCounter = ():void => {
+                    countersmall.value++
+                }
+    </script>
+    
+    
