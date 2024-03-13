0e3e210dac1a59570b122f903755e4a7fd026728
GF-SIS <info@grupofesa.co>
2024-03-12 20:25:06 -0500

    borrado helloworld y nuevo fenix

---
+++ fenix2/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1> Listado de Posts: </h1>
+  <PostContainer />
+</template>
+
+<script lang="ts" setup>
+import PostContainer from './components/PostContainer.vue';
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
